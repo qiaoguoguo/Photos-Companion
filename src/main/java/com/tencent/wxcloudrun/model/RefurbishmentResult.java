1b44dcbef2d7f4f7b5fb3da21a5d6fb9685cf7ae
zhangqiao <1456757829@qq.com>
2023-01-31 11:14:23 +0800

    异常查找

--- src/main/java/com/tencent/wxcloudrun/model/RefurbishmentResult.java
+++ src/main/java/com/tencent/wxcloudrun/model/RefurbishmentResult.java
@@ -7,10 +7,10 @@ import java.io.Serializable;
 @Data
 public class RefurbishmentResult implements Serializable {
 
-    private long completed_at;
-    private long created_at;
+    private Integer completed_at;
+    private Integer created_at;
     private String image;
-    private long processed_at;
+    private Integer processed_at;
     private Integer progress;
     private Integer return_type;
     private Integer state;
