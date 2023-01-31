5374bc3954e5141dcf17bf03a520cba0632b55ba
zhangqiao <1456757829@qq.com>
2023-01-31 11:16:47 +0800

    异常查找

--- src/main/java/com/tencent/wxcloudrun/config/ZtApiResponse.java
+++ src/main/java/com/tencent/wxcloudrun/config/ZtApiResponse.java
@@ -1,5 +1,6 @@
 package com.tencent.wxcloudrun.config;
 
+import com.tencent.wxcloudrun.model.RefurbishmentResult;
 import lombok.Data;
 
 @Data
@@ -8,24 +9,5 @@ public class ZtApiResponse<T> {
     private Integer status;
     private String message;
     private T data;
-
-    public static ZtApiResponse success(Object object){
-        ZtApiResponse resultVO = new ZtApiResponse();
-        resultVO.setData(object);
-        resultVO.setMessage("成功");
-        resultVO.setStatus(0);
-        return resultVO;
-    }
-
-    public static ZtApiResponse success(){
-        return success(null);
-    }
-
-    public static ZtApiResponse error(Integer code,String msg){
-        ZtApiResponse resultVO = new ZtApiResponse();
-        resultVO.setStatus(code);
-        resultVO.setMessage(msg);
-        return resultVO;
-    }
 
 }
