81b26105e821824683d2a850804cd2bc3f413071
zhangqiao <1456757829@qq.com>
2023-02-27 15:20:31 +0800

    修改cron表达式

--- src/main/java/com/tencent/wxcloudrun/cron/CaipiaoCron.java
+++ src/main/java/com/tencent/wxcloudrun/cron/CaipiaoCron.java
@@ -21,8 +21,16 @@ public class CaipiaoCron {
     private UserCaipiaoService userCaipiaoService;
 
     /** 每天自动查询所有用彩票中奖情况，自动推送 */
-    @Scheduled(cron = "0 20 15 ? * 2,4,7")
+    @Scheduled(cron = "0 30 15 ? * 1,3,6")
     public void configureTasks() {
+        Date date = new Date();
+        System.out.println(date+"大乐透开奖");
+        userCaipiaoService.kaijiang("14");
+    }
+
+    /** 每天自动查询所有用彩票中奖情况，自动推送 */
+    @Scheduled(cron = "0 0 23 ? * 1,3,6")
+    public void configureTasks1() {
         Date date = new Date();
         System.out.println(date+"大乐透开奖");
         userCaipiaoService.kaijiang("14");
